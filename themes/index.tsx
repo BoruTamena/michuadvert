add5795f90064d598f126f84688c5c6efd0a6d64
BoruTamena <borutamena46@gmail.com>
2024-05-28 05:11:26 -0400

    init

---
+++ themes/index.tsx
@@ -0,0 +1,52 @@
+
+'use client';
+import { createTheme } from "@mui/material";
+import {Poppins } from 'next/font/google';
+
+
+const poppins =Poppins({
+    weight: ['300', '400', '500', '700'],
+    subsets: ['latin'],
+    display: 'swap',
+  })
+
+export const theme=createTheme({
+
+    typography: {
+        fontFamily: poppins.style.fontFamily,
+      },
+
+    palette:{
+        primary:{
+           
+            main:"#CAF5FF"
+
+        },
+        secondary:{
+            main:"#F5DEDE",
+           
+        },
+        
+    
+    },
+
+    components:{
+        MuiPopover:{
+            styleOverrides:{
+                root:{
+                    
+                }
+            }
+        },
+        MuiPaper:{
+            styleOverrides:{
+                root:{
+                    backgroundColor:"primary",
+                    
+                }
+            }
+        }
+    }
+  
+    
+})
